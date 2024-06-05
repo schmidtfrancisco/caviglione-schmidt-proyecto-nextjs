2fff73d7117ce287c0881425bcc03a729995a249
Francisco Schmidt <franschmidt2415@gmail.com>
2024-06-04 23:11:32 -0300

    Continuar cambios home

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,15 @@ import Category from "./ui/home/Category";
 import Link from "next/link";
 import Image from "next/image";
 import homeImage from '../public/home.png'
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon, ArrowDownIcon } from "@heroicons/react/24/outline";
+import { buttonVariants } from "@/components/ui/button";
 
 
 export default async function Home() {
 
   return (
     <div className="">
-      
+
       <section className="w-full">
         <div className="container grid items-center justify-center px-4 md:px-6 md:grid-cols-2 lg:gap-2">
           <div className="flex flex-col items-center text-center gap-4 p-4 md:pl-6 md:items-start md:text-start">
@@ -21,7 +22,7 @@ export default async function Home() {
               Game<span className="text-sky-800 dark:text-gray-50">Store</span>
             </h1>
             <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              Explore nuesta amplia selección de juegos de mesa, videojuegos y juguetes para todas las edades. Encuentre el regalo perfecto o 
+              Explore nuesta amplia selección de juegos de mesa, videojuegos y juguetes para todas las edades. Encuentre el regalo perfecto o
               su juego preferido.
             </p>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
@@ -31,6 +32,15 @@ export default async function Home() {
               >
                 Explorar juegos
                 <ArrowRightIcon className="ml-2 h-4 w-4" />
+              </Link>
+              <Link
+                href="#categorias"
+                className={buttonVariants({
+                  variant: "outline",
+                })}
+              >
+                Ver categorías
+                <ArrowDownIcon className="ml-2 h-4 w-4" />
               </Link>
             </div>
           </div>
@@ -60,7 +70,7 @@ export default async function Home() {
         </Carousel>
       </section>
 
-      <section className="w-full py-10 md:py-14 lg:py-18 bg-gray-100 dark:bg-gray-800">
+      <section id="categorias" className="w-full py-10 md:py-14 lg:py-18 bg-gray-100 dark:bg-gray-800">
         <div className="container space-y-12 px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
@@ -81,60 +91,98 @@ export default async function Home() {
         </div>
       </section>
 
-      <section className="mb-12 mx-12 px-4 pt-6 border-t border-gray-100">
-        <h2 className="text-2xl font-bold mb-4">Juegos de mesa</h2>
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          orientation="horizontal"
-        >
-          <Suspense fallback={<CarouselGameSkeleton />}>
-            <GameCarouselContent category="Juegos de mesa" />
-          </Suspense>
+      <section className="w-full py-10 md:py-14 lg:py-18 dark:bg-gray-800">
+        <div className="container space-y-12 px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <div className="inline-block rounded-lg bg-green-800 px-3 py-1 text-sm text-white dark:bg-gray-800">
+                Juegos de mesa
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Sumérgete en Nuestra Colección Exclusiva</h2>
+              <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                Descubre una amplia variedad de juegos de mesa
+              </p>
+            </div>
+          </div>
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            orientation="horizontal"
+          >
+            <Suspense fallback={<CarouselGameSkeleton />}>
+              <GameCarouselContent category="Juegos de mesa" />
+            </Suspense>
 
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+        </div>
       </section>
 
-      <section className="mb-12 mx-12 px-4 pt-6 border-t border-gray-100">
-        <h2 className="text-2xl font-bold mb-4">Videojuegos</h2>
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          orientation="horizontal"
-        >
-          <Suspense fallback={<CarouselGameSkeleton />}>
-            <GameCarouselContent category="Videojuegos" />
-          </Suspense>
+      <section className="w-full py-10 border-t border-gray-100 md:py-14 lg:py-18 dark:bg-gray-800">
+        <div className="container space-y-12 px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <div className="inline-block rounded-lg bg-blue-600 px-3 py-1 text-sm text-white dark:bg-gray-800">
+                Videojuegos
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Adéntrate en el Mundo de los Videojuegos</h2>
+              <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                Explora nuestra colección de videojuegos para todas las plataformas
+              </p>
+            </div>
+          </div>
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            orientation="horizontal"
+          >
+            <Suspense fallback={<CarouselGameSkeleton />}>
+              <GameCarouselContent category="Videojuegos" />
+            </Suspense>
 
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+        </div>
       </section>
 
-      <section className="mb-12 mx-12 px-4 pt-6 border-t border-gray-100">
-        <h2 className="text-2xl font-bold mb-4">Juguetes</h2>
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          orientation="horizontal"
-        >
-          <Suspense fallback={<CarouselGameSkeleton />}>
-            <GameCarouselContent category="Juguetes" />
-          </Suspense>
+      <section className="w-full py-10 border-t border-gray-100 md:py-14 lg:py-18 dark:bg-gray-800">
+        <div className="container space-y-12 px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <div className="inline-block rounded-lg bg-orange-600 px-3 py-1 text-sm text-white dark:bg-gray-800">
+                Juguetes
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Descubre Nuestros Juguetes Únicos</h2>
+              <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                Encuentra el juguete perfecto para cada ocasión y edad
+              </p>
+            </div>
+          </div>
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            orientation="horizontal"
+          >
+            <Suspense fallback={<CarouselGameSkeleton />}>
+              <GameCarouselContent category="Juguetes" />
+            </Suspense>
 
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+        </div>
       </section>
     </div>
-
   );
 }
