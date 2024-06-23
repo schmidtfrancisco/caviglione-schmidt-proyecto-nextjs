15a5e4e06731d35309dcf9012034cb8219e86057
Ezequiel74452 <ezequielcaviglione@hotmail.com>
2024-06-23 15:51:20 -0300

    UI parte 2

--- components/inicio/CategoryCard.tsx
+++ components/inicio/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import GameCldImage from "@/components/juegos/GameCldImage";
+import { CategoryCldImage } from "@/components/juegos/GameCldImage";
 import { Category } from "@/lib/definitions/products-definitions";
 import { getCategoryLink } from "@/lib/utils";
 import Link from "next/link";
@@ -9,17 +9,17 @@ export default function CategoryCard({ category, description, src }: { category:
       <Link href={getCategoryLink(category)} className="absolute inset-0 z-10">
         <span className="sr-only">Explorar</span>
       </Link>
-      <div className="h-[200px] relative w-full">
-      <GameCldImage
-        src={src}
-        alt={category}
-        className="group-hover:scale-105 transition-transform duration-300 ease-in-out object-cover"
-      />
+      <div className="relative w-full h-48 sm:h-40 md:h-50 lg:h-60 xl:h-70">
+        <CategoryCldImage
+          src={src}
+          alt={category}
+          className="group-hover:scale-105 transition-transform duration-300 ease-in-out object-cover w-full h-full"
+        />
       </div>
       <div className="bg-white p-4 dark:bg-gray-950">
-        <h3 className="font-bold text-xl">{category}</h3>
+        <h3 className="font-bold text-lg md:text-xl">{category}</h3>
         <p className="text-sm text-gray-500 dark:text-gray-400">{description}</p>
       </div>
     </div>
-  )
+  );
 }
